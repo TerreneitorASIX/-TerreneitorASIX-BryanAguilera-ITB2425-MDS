cba189c2c15e5d85e842b3a7e03e7fc3de47cd6d
bryan.aguilera.7e6 <bryan.aguilera.7e6@itb.cat>
2024-09-26 10:21:52 +0200

    TA02

---
+++ TA02/ComprovaEdat.py
@@ -0,0 +1,16 @@
+"""
+
+Bryan Daniel Aguilera Nieto
+26/09/2024
+ASIXc MDS
+Descripció: Exemple TA02
+
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+
+if edat>=18:
+    print("Ets major d'edat")
+print("Programa Finalitzat")
